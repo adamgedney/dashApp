181aad3e16c2fbf791f25602e9b6d41041f44be9
Adam <adam.gedney@gmail.com>
2015-02-25 18:25:20 -0500

    init

---
+++ app/scripts/controllers/home.js
@@ -0,0 +1,21 @@
+
+//==========================================//
+//Home Controller
+//==========================================//
+
+App.controller('home', ['$scope', function($scope){
+
+
+	$scope.test = {
+		test : ''
+	};
+
+	$scope.title = 'my eggs'
+
+	$scope.in = 'foo bar';
+
+
+
+
+
+}]);
